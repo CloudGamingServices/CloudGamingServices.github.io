4cb1c0560bbbbc969476dd751e5b08ec1c804cb3
Cloud Gaming <149394072+CloudGamingServices@users.noreply.github.com>
2024-01-31 18:28:12 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality if needed
+// For example, displaying an alert when the page loads
+window.onload = function() {
+    alert('Welcome to Rick Grimes Fan Page!');
+};
